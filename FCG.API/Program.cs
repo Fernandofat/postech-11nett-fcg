b2e9c8cfdf38bf29d1bc680b16c9314518ac5bbc
Lucas Nunes <lucas.nunes@miketec.com.br>
2025-12-03 23:37:09 -0300

    Adicionado as descrições no Swagger.

--- FCG.API/Program.cs
+++ FCG.API/Program.cs
@@ -4,6 +4,7 @@ using FCG.API.Infra.Config;
 using FCG.API.Infra.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Title = "FIAP Cloud Games (FCG)",
+        Version = "V1",
+        Description =
+            "Plataforma de venda de jogos digitais e gestão de servidores para partidas online \n\n" +
+            "Contatos:\n\n" +
+            "• Fernando Ribeiro\n" +
+            "• João Paulo\n" +
+            "• Lucas Nunes RM 369391\n" +
+            "• Marcos Antonio RM 368502\n" +
+            "• Oberdan RM 369592\n",
+        Contact = new OpenApiContact
+        {
+            Name = "Equipe FCG",
+            Email = "teste@teste.com"
+        },
+
+    });
+});
 
 #region [JWT] Configuração do JWT
 
@@ -46,19 +69,10 @@ builder.Services.AddControllers();
 
 #endregion
 
-
 #region [DB] Configuração do Banco de Dados
 string postgresConnectionString = builder.Configuration.GetSection("Connections:PostgreSQL").Value;
 
-//builder.Services.AddHealthChecks()
-//    .AddNpgSql(
-//    postgresConnectionString,
-//    name: "PostgreSQL",
-//    failureStatus: HealthStatus.Unhealthy,
-//    tags: new[] { "db", "sql", "postgres" });
-
 #endregion
-
 
 #region [DI] Injeção de dependência
 builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("Connections"));
@@ -72,16 +86,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/V1/swagger.json", "FIAP Cloud Games (FCG) V1");
+    });
+
+    app.UseReDoc(c =>
+    {
+        c.DocumentTitle = "FIAP Cloud Games (FCG)";
+        c.SpecUrl = "/swagger/V1/swagger.json";
+    });
 }
-
-//#region [Healthcheck]
-//app.UseHealthChecks("/health", new HealthCheckOptions
-//{
-//    Predicate = _ => true,
-//    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
-
-//});
 
 app.UseLogMiddleware();
 
